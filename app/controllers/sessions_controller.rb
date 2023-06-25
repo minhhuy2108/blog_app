3b909984c1be9105356627ec9da08e4f68f23887
Minh <leminhhuy159753@gmail.com>
2023-06-25 16:46:37 +0700

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,18 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       reset_session
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+      render :new , status: :unprocessable_entity
+      # render turbo_stream: turbo_stream.update("flash", partial: "shared/errors",locals: { obj: @article })
     end
   end
 
     def destroy
-      log_out
+      log_out if logged_in?
       redirect_to root_url, status: :see_other
     end
 end
